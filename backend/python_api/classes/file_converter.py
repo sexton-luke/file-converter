79ac0aae68aab93111d593540f9a219a7a60bec9
Luke Sexton <lukejamesexton1995@gmail.com>
2023-11-10 10:02:14 +1000

    add file converter class

---
+++ backend/python_api/classes/file_converter.py
@@ -0,0 +1,30 @@
+from PIL import Image
+
+class FileConverter:
+    def __init__(self):
+        self.methods = {
+            ("jpg", "png"): self.jpg_to_png,
+            ("png", "jpg"): self.png_to_jpg,
+        }
+        
+    def get_supported_formats(self):
+        return self.methods.keys()
+
+    def convert(self, from_format, to_format, input_path, output_path):
+        key = (from_format, to_format)
+        if key in self.methods:
+            method = self.methods[key]
+            return method(input_path, output_path)
+        else:
+            return False  # Conversion not supported
+
+    def jpg_to_png(self, jpg_path, png_path):
+        image = Image.open(jpg_path)
+        image.save(png_path)
+        return (True, "image/png")
+
+    def png_to_jpg(self, png_path, jpg_path):
+        image = Image.open(png_path)
+        image.save(jpg_path)
+        return (True, "image/jpeg")
+
